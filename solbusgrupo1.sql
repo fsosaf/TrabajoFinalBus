d1bb12b10580e7005d6e22a658db62a7f8aa44ea
TinchoAcosta <soymartinacosta@hotmail.es>
2024-05-31 19:42:22 -0300

    Actualizacion: Base de Datos

--- solbusgrupo1.sql
+++ solbusgrupo1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-05-2024 a las 15:21:03
+-- Tiempo de generación: 01-06-2024 a las 00:40:49
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -20,41 +20,83 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `solbusgrupo1`
 --
+CREATE DATABASE IF NOT EXISTS `solbusgrupo1` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `solbusgrupo1`;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `colectivo`
+-- Estructura de tabla para la tabla `colectivos`
 --
 
-CREATE TABLE `colectivo` (
+CREATE TABLE `colectivos` (
   `ID_colectivo` int(11) NOT NULL,
-  `matricula` varchar(60) NOT NULL,
+  `matricula` varchar(7) NOT NULL,
   `marca` varchar(60) NOT NULL,
   `modelo` varchar(60) NOT NULL,
-  `capacidad` int(11) NOT NULL
+  `capacidad` int(11) NOT NULL,
+  `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `colectivos`
+--
+
+INSERT INTO `colectivos` (`ID_colectivo`, `matricula`, `marca`, `modelo`, `capacidad`, `estado`) VALUES
+(2, 'abc123', 'ford', '324', 30, 1);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `horario`
+-- Estructura de tabla para la tabla `horarios`
 --
 
-CREATE TABLE `horario` (
+CREATE TABLE `horarios` (
   `ID_horario` int(11) NOT NULL,
   `ID_ruta` int(11) NOT NULL,
   `hora_salida` time NOT NULL,
-  `hora_llegada` time NOT NULL
+  `hora_llegada` time NOT NULL,
+  `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `horarios`
+--
+
+INSERT INTO `horarios` (`ID_horario`, `ID_ruta`, `hora_salida`, `hora_llegada`, `estado`) VALUES
+(2, 4, '06:08:00', '06:08:00', 1),
+(3, 4, '13:08:00', '13:08:00', 0);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `pasaje`
+-- Estructura de tabla para la tabla `pasajeros`
 --
 
-CREATE TABLE `pasaje` (
+CREATE TABLE `pasajeros` (
+  `ID_pasajero` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `apellido` varchar(20) NOT NULL,
+  `dni` varchar(8) NOT NULL,
+  `correo` varchar(60) NOT NULL,
+  `telefono` varchar(60) NOT NULL,
+  `estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `pasajeros`
+--
+
+INSERT INTO `pasajeros` (`ID_pasajero`, `nombre`, `apellido`, `dni`, `correo`, `telefono`, `estado`) VALUES
+(2, 'juan', 'juarez', '45454545', 'juan@.com', '12345679', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pasajes`
+--
+
+CREATE TABLE `pasajes` (
   `ID_pasaje` int(11) NOT NULL,
   `ID_pasajero` int(11) NOT NULL,
   `ID_colectivo` int(11) NOT NULL,
@@ -65,70 +107,78 @@ CREATE TABLE `pasaje` (
   `precio` decimal(10,0) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Estructura de tabla para la tabla `pasajero`
+-- Volcado de datos para la tabla `pasajes`
 --
 
-CREATE TABLE `pasajero` (
-  `ID_pasajero` int(11) NOT NULL,
-  `nombre` varchar(60) NOT NULL,
-  `apellido` varchar(60) NOT NULL,
-  `dni` varchar(60) NOT NULL,
-  `correo` varchar(60) NOT NULL,
-  `telefono` varchar(60) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `pasajes` (`ID_pasaje`, `ID_pasajero`, `ID_colectivo`, `ID_ruta`, `fecha_viaje`, `hora_viaje`, `asiento`, `precio`) VALUES
+(2, 2, 2, 7, '1999-02-10', '13:08:00', 40, 9800),
+(3, 2, 2, 6, '2024-05-01', '19:12:16', 30, 10500);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `ruta`
+-- Estructura de tabla para la tabla `rutas`
 --
 
-CREATE TABLE `ruta` (
+CREATE TABLE `rutas` (
   `ID_ruta` int(11) NOT NULL,
   `origen` varchar(60) NOT NULL,
   `destino` varchar(60) NOT NULL,
-  `duracion_estimada` time NOT NULL
+  `duracion_estimada` time NOT NULL,
+  `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `rutas`
+--
+
+INSERT INTO `rutas` (`ID_ruta`, `origen`, `destino`, `duracion_estimada`, `estado`) VALUES
+(3, 'la punta', 'san luis', '00:00:00', 1),
+(4, 'la punta', 'san luis', '13:08:00', 1),
+(5, 'c', 'd', '16:11:52', 0),
+(6, 'f', 'e', '00:00:05', 1),
+(7, 'mendoza', 'cordoba', '00:00:00', 1);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `colectivo`
+-- Indices de la tabla `colectivos`
 --
-ALTER TABLE `colectivo`
-  ADD PRIMARY KEY (`ID_colectivo`);
+ALTER TABLE `colectivos`
+  ADD PRIMARY KEY (`ID_colectivo`),
+  ADD UNIQUE KEY `matricula` (`matricula`);
 
 --
--- Indices de la tabla `horario`
+-- Indices de la tabla `horarios`
 --
-ALTER TABLE `horario`
+ALTER TABLE `horarios`
   ADD PRIMARY KEY (`ID_horario`),
-  ADD UNIQUE KEY `ID_ruta` (`ID_ruta`);
+  ADD KEY `ID_ruta` (`ID_ruta`);
 
 --
--- Indices de la tabla `pasaje`
+-- Indices de la tabla `pasajeros`
 --
-ALTER TABLE `pasaje`
+ALTER TABLE `pasajeros`
+  ADD PRIMARY KEY (`ID_pasajero`),
+  ADD UNIQUE KEY `dni` (`dni`),
+  ADD UNIQUE KEY `correo` (`correo`);
+
+--
+-- Indices de la tabla `pasajes`
+--
+ALTER TABLE `pasajes`
   ADD PRIMARY KEY (`ID_pasaje`),
-  ADD UNIQUE KEY `ID_pasajero` (`ID_pasajero`),
-  ADD UNIQUE KEY `ID_colectivo` (`ID_colectivo`),
-  ADD UNIQUE KEY `ID_ruta` (`ID_ruta`);
+  ADD KEY `ID_pasajero` (`ID_pasajero`),
+  ADD KEY `ID_ruta` (`ID_ruta`),
+  ADD KEY `ID_colectivo` (`ID_colectivo`);
 
 --
--- Indices de la tabla `pasajero`
+-- Indices de la tabla `rutas`
 --
-ALTER TABLE `pasajero`
-  ADD PRIMARY KEY (`ID_pasajero`);
-
---
--- Indices de la tabla `ruta`
---
-ALTER TABLE `ruta`
+ALTER TABLE `rutas`
   ADD PRIMARY KEY (`ID_ruta`);
 
 --
@@ -136,52 +186,52 @@ ALTER TABLE `ruta`
 --
 
 --
--- AUTO_INCREMENT de la tabla `colectivo`
+-- AUTO_INCREMENT de la tabla `colectivos`
 --
-ALTER TABLE `colectivo`
-  MODIFY `ID_colectivo` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `colectivos`
+  MODIFY `ID_colectivo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT de la tabla `horario`
+-- AUTO_INCREMENT de la tabla `horarios`
 --
-ALTER TABLE `horario`
-  MODIFY `ID_horario` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `horarios`
+  MODIFY `ID_horario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT de la tabla `pasaje`
+-- AUTO_INCREMENT de la tabla `pasajeros`
 --
-ALTER TABLE `pasaje`
-  MODIFY `ID_pasaje` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `pasajeros`
+  MODIFY `ID_pasajero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT de la tabla `pasajero`
+-- AUTO_INCREMENT de la tabla `pasajes`
 --
-ALTER TABLE `pasajero`
-  MODIFY `ID_pasajero` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `pasajes`
+  MODIFY `ID_pasaje` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT de la tabla `ruta`
+-- AUTO_INCREMENT de la tabla `rutas`
 --
-ALTER TABLE `ruta`
-  MODIFY `ID_ruta` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `rutas`
+  MODIFY `ID_ruta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `horario`
+-- Filtros para la tabla `horarios`
 --
-ALTER TABLE `horario`
-  ADD CONSTRAINT `horario_ibfk_1` FOREIGN KEY (`ID_ruta`) REFERENCES `ruta` (`ID_ruta`);
+ALTER TABLE `horarios`
+  ADD CONSTRAINT `horarios_ibfk_1` FOREIGN KEY (`ID_ruta`) REFERENCES `rutas` (`ID_ruta`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
--- Filtros para la tabla `pasaje`
+-- Filtros para la tabla `pasajes`
 --
-ALTER TABLE `pasaje`
-  ADD CONSTRAINT `pasaje_ibfk_1` FOREIGN KEY (`ID_colectivo`) REFERENCES `colectivo` (`ID_colectivo`),
-  ADD CONSTRAINT `pasaje_ibfk_2` FOREIGN KEY (`ID_pasajero`) REFERENCES `pasajero` (`ID_pasajero`),
-  ADD CONSTRAINT `pasaje_ibfk_3` FOREIGN KEY (`ID_ruta`) REFERENCES `ruta` (`ID_ruta`);
+ALTER TABLE `pasajes`
+  ADD CONSTRAINT `pasajes_ibfk_1` FOREIGN KEY (`ID_pasajero`) REFERENCES `pasajeros` (`ID_pasajero`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `pasajes_ibfk_2` FOREIGN KEY (`ID_ruta`) REFERENCES `rutas` (`ID_ruta`),
+  ADD CONSTRAINT `pasajes_ibfk_3` FOREIGN KEY (`ID_colectivo`) REFERENCES `colectivos` (`ID_colectivo`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
